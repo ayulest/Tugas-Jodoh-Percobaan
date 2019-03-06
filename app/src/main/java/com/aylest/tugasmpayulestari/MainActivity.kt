7e507090ee8481d3ab01e11c12f7b8afdc9321d3
ayulest <ayyu.lest6@gmail.com>
2019-03-06 20:55:17 +0700

    update

---
+++ app/src/main/java/com/aylest/tugasmpayulestari/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.aylest.tugasmpayulestari
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnMatch.setOnClickListener { randomJodoh() }
+    }
+
+    fun randomJodoh() {
+        val randomCewek = Random().nextInt(4)+1
+        val randomCowok = Random().nextInt(4)+1
+        val drawableResCewek = when(randomCewek){
+            1 ->R.drawable.cewek1
+            2 ->R.drawable.cewek2
+            3 ->R.drawable.cewek3
+            else ->R.drawable.cewek4
+        }
+        val drawableResCowok = when(randomCowok){
+            1 ->R.drawable.cowok1
+            2 ->R.drawable.cowok2
+            3 ->R.drawable.cowok3
+            else ->R.drawable.cowok4
+
+        }
+
+        ivDadu1.setImageResource(drawableResCewek)
+        ivDadu2.setImageResource(drawableResCowok)
+    }
+}
